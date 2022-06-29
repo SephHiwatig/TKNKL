ebcf0347fcaa5ab151d505ac28cb3551c39272f9
WilfredoJoseph Hiwatig <jhiwatig.dev@gmail.com>
2022-06-29 19:56:46 -0400

    Added smooth scroll to top on link click

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,6 +3,7 @@ import styles from "./Footer.module.css";
 import { BsFacebook, BsInstagram } from "react-icons/bs";
 import shopTemplate from "./../../assets/shop_template.jpg";
 import tknkl from "./../../assets/tknkl_white.jpg";
+import { Link } from "react-router-dom";
 
 function Footer(props) {
   const [width, setWidth] = useState(600);
@@ -24,11 +25,15 @@ function Footer(props) {
   return (
     <div className={props.home ? styles.WrapperHome : styles.Wrapper}>
       <div className={styles.ShopName}>
-        <h1>
+        {/* <h1>
           <img className={styles.Shop} src={shopTemplate} alt="logo" />
-        </h1>
+        </h1> */}
         <h1>
-          <img className={styles.Title} src={tknkl} alt="logo" />
+          {/* <img className={styles.Title} src={tknkl} alt="logo" />
+           */}
+          <Link to="" className={styles.TknklLink} onClick={() => {window.scrollTo({top: 0, behavior: "smooth"})}}>
+            <h1>TKNKL</h1>
+          </Link>
         </h1>
         <h3>4895 Saint-Charles Blvd, Pierrefonds, QC H9H 3E4</h3>
         <div>
--- src/components/Navigation/Nav.js
+++ src/components/Navigation/Nav.js
@@ -7,13 +7,16 @@ function Nav() {
   return (
     <div className={styles.Navbar}>
       <ul className={styles.ListItemWrapper}>
-        <Link className={styles.ListItem} to="TattooPortfolio">
+        <Link className={styles.ListItem} to="TattooPortfolio"
+          onClick={() => {window.scrollTo({top: 0, behavior: "smooth"})}}>
           <li>Tattoos</li>
         </Link>
-        <Link className={styles.ListItem} to="Arts">
+        <Link className={styles.ListItem} to="Arts"
+          onClick={() => {window.scrollTo({top: 0, behavior: "smooth"})}}>
           <li>Arts</li>
         </Link>
-        <Link className={styles.ListItem} to="Booking">
+        <Link className={styles.ListItem} to="Booking"
+          onClick={() => {window.scrollTo({top: 0, behavior: "smooth"})}}>
           <li>Booking</li>
         </Link>
       </ul>
